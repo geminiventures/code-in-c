d837b1a6fd732c2bc5758778fef008e4559493d8
Mark <mark@geminiventures.io>
2026-03-07 06:05:01 -0500

    Object oriented structs

---
+++ structs_as_obj/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "counter.h"
+
+int main() {
+    // Read input
+    int starting_value;
+    int num_increments;
+    scanf("%d", &starting_value);
+    scanf("%d", &num_increments);
+    
+    // TODO: Create a Counter and initialize it with starting_value
+    Counter counter = { .value = starting_value };
+    // TODO: Call counter_increment the specified number of times
+    for (int i = 0; i < num_increments; i++) {
+        counter_increment(&counter);
+    }
+    // TODO: Use counter_get to retrieve the final value and print it
+    // Print format: "Final value: {result}"
+    printf("Final value: %d\n", counter_get(&counter));
+
+    return 0;
+}
